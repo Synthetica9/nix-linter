cc20ddd53a96b674b6b824847d41a1acd331bbac
Patrick Hilhorst <git@hilhorst.be>
2018-11-23 14:17:55 +0100

    Basic functionality with Streamly

--- default.nix
+++ default.nix
@@ -1,6 +1,13 @@
 { pkgs ? import <nixpkgs> {} }:
+with pkgs;
 
-pkgs.haskellPackages.extend (pkgs.haskell.lib.packageSourceOverrides {
+(haskellPackages.override ({
+    overrides = self: super: {
+      streamly = super.streamly_0_5_2;
+    };
+})).extend (haskell.lib.packageSourceOverrides {
   nix-linter = ./.;
 })
-// { inherit pkgs; }
+// {
+  inherit pkgs;
+}
