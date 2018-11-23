1096d78824d8063fa69ff5b6755e4c3f0ffb7ee5
Patrick Hilhorst <git@hilhorst.be>
2018-11-24 00:07:00 +0100

    Fixup dependencies

--- default.nix
+++ default.nix
@@ -4,6 +4,7 @@ with pkgs;
 (haskellPackages.override ({
     overrides = self: super: {
       streamly = super.streamly_0_5_2;
+      path-io = super.path-io_1_4_0;
     };
 })).extend (haskell.lib.packageSourceOverrides {
   nix-linter = ./.;
