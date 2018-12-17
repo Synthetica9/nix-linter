923ed642f9eb4419846ef8b9658062dc1b6aa69e
Patrick Hilhorst <git@hilhorst.be>
2018-12-17 18:09:19 +0100

    Check empty variadic param set (#22)
    * add checkEmptyVariadicParamSet [WIP]
    
    * More [WIP]
    
    * add checkEmptyVariadicParamSet

--- update_readme.sh
+++ update_readme.sh
@@ -4,4 +4,7 @@
 # See https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 set -euxo pipefail
 
+# Fixes troubles with Travis WRT insufficient disk space
+unset TMPDIR
+
 gpp -H -x README.md.gpp -o README.md
