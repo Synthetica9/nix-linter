de21c9680dcb8b281658f1d7a8a27b91c839265b
Patrick Hilhorst <git@hilhorst.be>
2019-01-04 17:15:21 +0100

    Use updated streamly

--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@ with pkgs;
 
 (haskellPackages.override ({
     overrides = self: super: {
-      streamly = super.streamly_0_5_2 or super.streamly_0_5_0;
+      streamly = super.streamly_0_5_2 or super.streamly_0_5_0 or super.streamly;
       path-io = super.path-io_1_4_0 or super.path-io;
     };
 })).extend (haskell.lib.packageSourceOverrides {
