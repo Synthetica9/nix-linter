e13e243ae1c88c54711e81dc3a524e022c4985c4
Patrick Hilhorst <git@hilhorst.be>
2019-01-04 16:57:07 +0100

    Use updated path-io

--- default.nix
+++ default.nix
@@ -4,7 +4,7 @@ with pkgs;
 (haskellPackages.override ({
     overrides = self: super: {
       streamly = super.streamly_0_5_2 or super.streamly_0_5_0;
-      path-io = super.path-io_1_4_0;
+      path-io = super.path-io_1_4_0 or super.path-io;
     };
 })).extend (haskell.lib.packageSourceOverrides {
   nix-linter = ./.;
