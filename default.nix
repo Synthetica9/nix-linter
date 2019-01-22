699a253a1f09b1d06136e6862e1dabfecdf00e3a
Mario Rodas <marsam@users.noreply.github.com>
2019-01-22 00:19:32 -0500

    Set working nixpkgs hash into default.nix

--- default.nix
+++ default.nix
@@ -1,10 +1,14 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import (builtins.fetchTarball {
+    url = https://github.com/NixOS/nixpkgs/archive/e6d726e5aa527e6f41ec06adeb7f0272294373c8.tar.gz;
+    sha256 = "0s09nfcl245pyyyqi9q8m1lfaiqms5sm81lcpmvvsh5988iylcfx";
+  }) { }
+}:
 with pkgs;
 
 (haskellPackages.override ({
     overrides = self: super: {
       streamly = super.streamly_0_5_2 or super.streamly_0_5_0 or super.streamly;
-      path-io = super.path-io_1_4_0 or super.path-io;
+      path-io = super.path-io_1_4_1 or super.path-io;
     };
 })).extend (haskell.lib.packageSourceOverrides {
   nix-linter = ./.;
